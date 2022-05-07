bb1df0ee946c8ffe8373e9aefdda02e0f8f995d3
Deepudeepthi555 <deepikamohandeepz99@gmail.com>
2022-05-07 17:10:23 +0530

    added expenses

---
+++ expenses/script.js
@@ -0,0 +1,24 @@
+
+    function Calculate()
+    {
+        var house = document.getElementById("housing").value;
+        var  tran= document.getElementById("transport").value;
+        var foo=document.getElementById("food").value;
+        var util=document.getElementById("Utility").value;
+        var ce=document.getElementById("cell").value;
+        var chi=document.getElementById("child").value;
+        var clo=document.getElementById("cloth").value;
+        var he=document.getElementById("health").value;
+        var lf=document.getElementById("life").value;
+        var su=document.getElementById("sub").value;
+        var hom=document.getElementById("home").value;
+        var ent=document.getElementById("entertain").value;
+        var lo=document.getElementById("loan").value;
+        var cre=document.getElementById("credit").value;
+        var emer=document.getElementById("emergency").value;
+        var lar=document.getElementById("large").value;
+        var sum=house+tran;
+        alert(sum);    
+
+        
+    }
